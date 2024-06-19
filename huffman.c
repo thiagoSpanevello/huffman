b200414ee54711d4518c6c36f4b8c54f95dc6ab3
luigiPisoni <luigipisoni04@gmail.com>
2024-06-19 02:20:54 -0300

    complete huffman, starting codification

--- huffman.c
+++ huffman.c
@@ -9,10 +9,11 @@ typedef struct node
     int frequency;
     struct node *right;
     struct node *left;
-} node;
+} Node;
 
-node* makeNode(char character, int frequency) {
-    node* newNode = (node*)malloc(sizeof(node));
+Node *makeNode(char character, int frequency)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node));
     newNode->character = character;
     newNode->frequency = frequency;
     newNode->left = NULL;
@@ -20,56 +21,78 @@ node* makeNode(char character, int frequency) {
     return newNode;
 }
 
-
-void bubbleSort(node** array, int size) {
-     for (int i = 0; i < size-1; i++)
+void bubbleSort(Node **array, int size)
+{
+    // Ordenação por frequência.
+    for (int i = 0; i < size - 1; i++)
     {
-        for (int j = 0; j < size-i-1; j++)
+        for (int j = 0; j < size - i - 1; j++)
         {
-            if (array[j]->frequency > array[j+1]->frequency)
+            if (array[j]->frequency > array[j + 1]->frequency)
             {
-                node* aux = array[j];
-                array[j] = array[j+1];
-                array[j+1] = aux;
+                Node *aux = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = aux;
             }
         }
-    }  
+    }
+
+    // Ordenação alfabética, mantendo a ordem de frequência.
     for (int i = 1; i < size; i++)
     {
-        for (int j = 0; j < size-1; j++)
+        for (int j = 0; j < size - 1; j++)
         {
-            if (array[j]->frequency == array[j+1]->frequency && array[j]->character > array[j+1]->character)
+            if (array[j]->frequency == array[j + 1]->frequency && array[j]->character > array[j + 1]->character)
             {
-                node* aux = array[j];
-                array[j] = array[j+1];
-                array[j+1] = aux;
-            } 
+                Node *aux = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = aux;
+            }
         }
     }
 }
 
-
-node* makeHuffman(node** array, int size) {
-    while (size > 1) {
+Node *makeHuffman(Node **array, int size)
+{
+    while (size > 1)
+    {
         bubbleSort(array, size);
 
-        node* left = array[0];
-        node* right = array[1];
+        Node *left = array[0];
+        Node *right = array[1];
 
-        node* newNode = makeNode('\0', left->frequency + right->frequency);
+        Node *newNode = makeNode('\0', left->frequency + right->frequency);
         newNode->left = left;
         newNode->right = right;
 
-        for (int i = 2; i < size; i++) {
-            array[i - 2] = array[i];
+        array[0] = newNode; // O primeiro elemento do vetor é substituído pelo novo nó.
+        for (int i = 2; i < size; i++)
+        {
+            array[i - 1] = array[i];
         }
-        array[size - 2] = newNode;
+        array[size - 1] = NULL;
         size--;
     }
     return array[0];
 }
 
-
+void buscaLetra(Node *node, char c, char **codification, int i)
+{
+    if (node->right)
+    {
+        // add 1 na codificação
+        codification[i] = '1';
+        i++;
+        return buscaLetra(node->right, c, codification, i);
+    }
+    else if (node->left)
+    {
+        // add 0 na codificação
+        codification[i] = '0';
+        i++;
+        return buscaLetra(node->left, c, codification, i);
+    }
+}
 
 int main()
 {
@@ -78,16 +101,17 @@ int main()
     {
         exit(1);
     }
-    char characters[SIZE] = {0};
+
+    char characters[SIZE] = {0}, *codification[SIZE] = {0};
     int frequencies[SIZE] = {0};
+
     char c = fgetc(amostra);
-    int flag = 0;
-    int j = 0;
-    int numCharacters = 0;
+    int flag = 0, uniqueCharacters = 0, numCharacters = 0;
+
     while (!feof(amostra))
     {
         flag = 0;
-        for (int i = 0; i < j; i++)
+        for (int i = 0; i < uniqueCharacters; i++)
         {
             if (c == characters[i])
             {
@@ -102,26 +126,35 @@ int main()
             continue;
         }
         numCharacters++;
-        characters[j] = c;
-        frequencies[j]++;
-        j++;
+        characters[uniqueCharacters] = c;
+        frequencies[uniqueCharacters]++;
+        uniqueCharacters++;
         c = fgetc(amostra);
     }
-    node* arrayNodes[numCharacters];
-    for (int i = 0; i < j; i++)
+    Node *arrayNodes[numCharacters];
+
+    // Criação dos nós, com a inserção de seu caracter e frequência.
+    for (int i = 0; i < uniqueCharacters; i++)
     {
-        // printf("caractere: %c, frequencia: %d\n", characters[i], frequencies[i]);
         arrayNodes[i] = makeNode(characters[i], frequencies[i]);
     }
+
+    // Ordenação inicial do vetor.
     bubbleSort(arrayNodes, numCharacters);
-    
-    for (int i = 0; i < j; i++)
+
+    // Criação da árvore de Huffman.
+    Node *root = makeHuffman(arrayNodes, numCharacters);
+    // printf("%c", root->right->right->right->character);
+
+    rewind(amostra); // Volta o leitor da pasta ao seu início.
+    c = fgetc(amostra);
+
+    // Percorre toda a pasta novamente, até encontrar uma quebra de linha.
+    while (!feof(amostra))
     {
-        printf("caractere: %c, frequencia: %d\n", arrayNodes[i]->character, arrayNodes[i]->frequency);
+        buscaLetra(root, c, codification, 0);
+        c = fgetc(amostra);
     }
-    // node* root = makeHuffman(arrayNodes, numCharacters);
-    // printf("",root);
 
+    printf("%s", codification);
 }
-
-
